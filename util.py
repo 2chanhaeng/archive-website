2249f9ed0bbdc904066064a27692683b03311396
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-08-18 21:05:14 +0900

    Create makedirs function

---
+++ util.py
@@ -0,0 +1,18 @@
+if "import":
+    if "type hint":
+        from typing import Callable
+        from bs4 import Tag
+        
+    if "built-in":
+        import os
+        from urllib import parse
+    
+    if "external":
+        pass
+
+    if "personal": 
+        from fp import curry
+
+__all__ = ["makedirs", "url_to_path", "add_domain", "get_href", "has_href"]
+
+makedirs:Callable[[str], None] = lambda path: os.makedirs(path, exist_ok=True)
