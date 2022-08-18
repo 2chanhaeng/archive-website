55ba043bd7a553522abd64fee3b18339c5367ee1
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-08-18 21:05:38 +0900

    Create url_to_path function

--- util.py
+++ util.py
@@ -16,3 +16,8 @@ if "import":
 __all__ = ["makedirs", "url_to_path", "add_domain", "get_href", "has_href"]
 
 makedirs:Callable[[str], None] = lambda path: os.makedirs(path, exist_ok=True)
+
+
+def url_to_path(url:str) -> str:
+    parsed = parse.urlparse(url)
+    return "saved/" + parsed.netloc + parsed.path
